4b227afd091ecb98b446924e2c5a94d577345b14
sanjuverma373 <sv0240866@gmail.com>
2024-02-28 11:04:05 +0530

    Headersectionmodified

--- src/components/Headersection.jsx
+++ src/components/Headersection.jsx
@@ -10,10 +10,10 @@ const Headersection = () => {
     <div className=' overflow-hidden bg-[url(./assets/webps/bgheaderimg.webp)] lg:bg-full bg-cover bg-center bg-repeat xl:min-h-screen sm:h-[730px] md:h-[874px] lg:h-[810px] h-[660px] relative z-50'>
       <Navsection/>
       <div className=' max-w-[935px] px-3 mx-auto w-full'>
-        <img className=' object-cover absolute bottom-[6%] right-[10%] lg:max-w-[211px] sm:max-w-[140px] max-w-[100px]  animate-flip w-full z-10 ' src={acycled} alt="#" />
+        <img className=' object-cover absolute bottom-[6%] right-[10%] lg:max-w-[183px] xl:max-w-[211px] sm:max-w-[140px] max-w-[100px]  animate-flip w-full z-10 ' src={acycled} alt="#" />
         <img className=' object-cover absolute bottom-[15%] right-[43%] -z-10 md:max-w-[131px] sm:max-w-[65px] max-w-[70px] animate-flip1 w-full' src={cycled} alt="#" />
         <div className=' object-cover grid lg:grid-cols-2 gap-[30px] lg:gap-[100px] pt-[83px]'>
-             <div className=' flex justify-center lg:justify-start items-start lg:items-center order-2 lg:order-1 lg:pt-[100px]  '>
+             <div className=' flex justify-center lg:justify-start items-start lg:items-center order-2 lg:order-1 lg:pt-[90px]  '>
                 <img className=' object-cover w-full max-w-[300.14px] sm:max-w-[394.14px] h-full min-h-[170.86px] sm:min-h-[240.86px] z-10' src={arod} alt="#" />
              </div>
              <div className=' order-1 lg:order-2 flex lg:flex-col justify-center lg:justify-start '>
